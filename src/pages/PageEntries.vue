2046be7457e5b63dfe4f0f82503eb16889e3bca9
Leonardo Naime <leonardo.naime@exati.com.br>
2025-06-05 11:16:21 -0300

    Divisão PageEntries em componentes e adição NothingHere quando não tem Entries

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,43 +1,22 @@
 <template>
   <q-page>
     <div class="q-pa-md">
+
+      <NothingHere
+        v-if="!storeEntries.entries.length"
+      />
+
       <q-list
+        v-else
         bordered
         separator
       >
 
-        <q-slide-item
+        <Entry 
           v-for="entry in storeEntries.entries"
           :key="entry.id"
-          @right="onEntrySlideRight($event, entry)"
-          left-color="positive"
-          right-color="negative"
-        >
-          <!-- <template v-slot:left>
-            <q-icon name="done" />
-          </template> -->
-          <template v-slot:right>
-            <q-icon name="delete" />
-          </template>
-
-          <q-item>
-            <q-item-section
-              class="text-weight-bold"
-              :class="useAmountColorClass(entry.amount)"
-            >
-              {{ entry.name }}
-            </q-item-section>
-
-            <q-item-section
-              class="text-weight-bold"
-              :class="useAmountColorClass(entry.amount)"
-              side
-            >
-              {{ useCurrencify(entry.amount) }}
-            </q-item-section>
-          </q-item>
-
-        </q-slide-item>
+          :entry="entry"
+        />
 
       </q-list>
     </div>
@@ -45,52 +24,8 @@
     <q-footer
       class="bg-transparent"
     >
-      <div class="row q-mb-sm q-px-md q-py-sm shadow-up-3">
-        <div class="col text-grey-7 text-h6">
-          Balance:
-        </div>
-        <div
-          :class="useAmountColorClass(storeEntries.balance)"
-          class="col text-h6 text-right"
-        >
-          {{ useCurrencify(storeEntries.balance) }}
-        </div>
-      </div>
-      <q-form
-        @submit="addEntryFormSubmit"
-        class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary"
-      >
-        <div class="col">
-          <q-input
-            v-model="addEntryForm.name"
-            ref="nameRef"
-            placeholder="Name"
-            bg-color="white"
-            outlined
-            dense
-          />
-        </div>
-        <div class="col">
-          <q-input
-            v-model.number="addEntryForm.amount"
-            input-class="text-right"
-            placeholder="Amount"
-            bg-color="white"
-            type="number"
-            step="0.01"
-            outlined
-            dense
-          />
-        </div>
-        <div class="col col-auto">
-          <q-btn
-            color="primary"
-            icon="add"
-            type="submit"
-            round
-          />
-        </div>
-      </q-form>
+      <Balance />
+      <AddEntry />
     </q-footer>
   </q-page>
 </template>
@@ -101,11 +36,11 @@
     imports
   */
   
-    import { ref, reactive } from 'vue'
-    import { useQuasar } from 'quasar'
     import { useStoreEntries } from 'src/stores/storeEntries'
-    import { useCurrencify } from 'src/use/useCurrencify'
-    import { useAmountColorClass } from 'src/use/useAmountColorClass'
+    import Balance from 'src/components/Entries/Balance.vue'
+    import AddEntry from 'src/components/Entries/AddEntry.vue'
+    import Entry from 'src/components/Entries/Entry.vue'
+    import NothingHere from 'src/components/Entries/NothingHere.vue'
 
 
   /*
@@ -113,71 +48,5 @@
   */
   
     const storeEntries = useStoreEntries()
-
-
-  /*
-    quasar
-  */
-  
-    const $q = useQuasar()  
-
-
-  /*
-    add entry
-  */
-
-    const nameRef = ref(null)
-
-    const addEntryFormDefault = {
-      name: '',
-      amount: null
-    }
-
-    const addEntryForm = reactive({
-      ...addEntryFormDefault
-    })
-
-    const addEntryFormReset = () => {
-      Object.assign(addEntryForm, addEntryFormDefault)
-      nameRef.value.focus()
-    }
-
-    const addEntryFormSubmit = () => {
-      storeEntries.addEntry(addEntryForm)
-      addEntryFormReset()
-    }
-  
-
-  /*
-    slide items
-  */
-  
-    const onEntrySlideRight = ({ reset }, entry) => {
-      $q.dialog({
-        title: 'Delete Entry',
-        message: `
-          Delete this entry?
-          <div class="q-mt-md text-weight-bold ${ useAmountColorClass(entry.amount) }">
-            ${ entry.name } : ${ useCurrencify(entry.amount) }
-          </div>
-        `,
-        cancel: true,
-        persistent: true,
-        html: true,
-        ok: {
-          label: 'Delete',
-          color: 'negative',
-          noCaps: true
-        },
-        cancel: {
-          color: 'primary',
-          noCaps: true
-        }
-      }).onOk(() => {
-        storeEntries.deleteEntry(entry.id)
-      }).onCancel(() => {
-        reset()
-      })
-    }
 
 </script>
