ac3fed170f86ef9741ab30cff7ef2c1ec567446f
Leonardo Naime <leonardo.naime@exati.com.br>
2025-06-05 08:35:54 -0300

    Adição deslizar para deletar e notificação na deleção de um item

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -5,25 +5,40 @@
         bordered
         separator
       >
-        <q-item
-          v-for="entry in entries"
-          :key="entry.id"
-        >
-          <q-item-section
-            class="text-weight-bold"
-            :class="useAmountColorClass(entry.amount)"
-          >
-            {{ entry.name }}
-          </q-item-section>
 
-          <q-item-section
-            class="text-weight-bold"
-            :class="useAmountColorClass(entry.amount)"
-            side
-          >
-            {{ useCurrencify(entry.amount) }}
-          </q-item-section>
-        </q-item>
+      <q-slide-item
+        v-for="entry in entries"
+        :key="entry.id"
+        @right="onEntrySlideRight($event, entry)"
+        left-color="positive"
+        right-color="negative"
+      >
+        <!-- <template v-slot:left>
+          <q-icon name="done" />
+        </template> -->
+        <template v-slot:right>
+          <q-icon name="delete" />
+        </template>
+
+          <q-item>
+            <q-item-section
+              class="text-weight-bold"
+              :class="useAmountColorClass(entry.amount)"
+            >
+              {{ entry.name }}
+            </q-item-section>
+
+            <q-item-section
+              class="text-weight-bold"
+              :class="useAmountColorClass(entry.amount)"
+              side
+            >
+              {{ useCurrencify(entry.amount) }}
+            </q-item-section>
+          </q-item>
+
+        </q-slide-item>
+
       </q-list>
     </div>
 
@@ -87,9 +102,16 @@
   */
   
     import { ref, computed, reactive } from 'vue'
-    import { uid } from 'quasar'
+    import { uid, useQuasar } from 'quasar'
     import { useCurrencify } from 'src/use/useCurrencify'
     import { useAmountColorClass } from 'src/use/useAmountColorClass'
+
+
+  /*
+    quasar
+  */
+  
+    const $q = useQuasar()  
 
 
   /*
@@ -157,6 +179,52 @@
       addEntryFormReset()
     }
   
+
+  /*
+    slide items
+  */
+  
+    const onEntrySlideRight = ({ reset }, entry) => {
+      $q.dialog({
+        title: 'Delete Entry',
+        message: `
+          Delete this entry?
+          <div class="q-mt-md text-weight-bold ${ useAmountColorClass(entry.amount) }">
+            ${ entry.name } : ${ useCurrencify(entry.amount) }
+          </div>
+        `,
+        cancel: true,
+        persistent: true,
+        html: true,
+        ok: {
+          label: 'Delete',
+          color: 'negative',
+          noCaps: true
+        },
+        cancel: {
+          color: 'primary',
+          noCaps: true
+        }
+      }).onOk(() => {
+        deleteEntry(entry.id)
+      }).onCancel(() => {
+        reset()
+      })
+    }
+
+
+  /*
+    delete entry
+  */
+  
+    const deleteEntry = entryId => {
+      const index = entries.value.findIndex(entry => entry.id === entryId)
+      entries.value.splice(index, 1)
+      $q.notify({
+        message: 'Entry deleted',
+        position: 'top'
+      })
+    }
     
 
 </script>
