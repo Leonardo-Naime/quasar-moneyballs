6cb5c177def637602c4d4ddc3d25e4be494c80c7
Leonardo Naime <leonardo.naime@exati.com.br>
2025-06-25 11:57:58 -0300

    Uso LocalStorage para salvar settings e entries localmente. Correção bug delete quando usava ordenação com 2 elementos

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+import { useStoreSettings } from 'src/stores/storeSettings'
+import { useStoreEntries } from 'src/stores/storeEntries'
+
 defineOptions({
   name: 'App'
 });
+
+const storeSettings = useStoreSettings(),
+      storeEntries = useStoreEntries()
+
+onMounted(() => {
+  storeSettings.loadSettings()
+  storeEntries.loadEntries()
+})
 
 // window.addEventListener('contextmenu', e => {
 //   e.preventDefault()
--- src/stores/storeEntries.js
+++ src/stores/storeEntries.js
@@ -1,39 +1,43 @@
 import { defineStore } from 'pinia'
-import { ref, computed, reactive } from 'vue'
-import { uid, Notify } from 'quasar'
+import { ref, computed, reactive, watch, nextTick } from 'vue'
+import { uid, Notify, LocalStorage } from 'quasar'
 
 export const useStoreEntries = defineStore('entries', () => {
 
   /*
     state
   */
-
+  
     const entries = ref([
-      {
-        id: 'id1',
-        name: 'Salary',
-        amount: 4999.99,
-        paid: false
-      },
-      {
-        id: 'id2',
-        name: 'Rent',
-        amount: -999,
-        paid: false
-      },
-      {
-        id: 'id3',
-        name: 'Phone bill',
-        amount: -14.99,
-        paid: false
-      },
-      {
-        id: 'id4',
-        name: 'Unknown',
-        amount: 0,
-        paid: false
-      },
+      // {
+      //   id: 'id1',
+      //   name: 'Salary',
+      //   amount: 4999.99,
+      //   paid: true
+      // },
+      // {
+      //   id: 'id2',
+      //   name: 'Rent',
+      //   amount: -999,
+      //   paid: false
+      // },
+      // {
+      //   id: 'id3',
+      //   name: 'Phone bill',
+      //   amount: -14.99,
+      //   paid: false
+      // },
+      // {
+      //   id: 'id4',
+      //   name: 'Unknown',
+      //   amount: 0,
+      //   paid: false
+      // },
     ])
+
+    watch(entries.value, () => {
+      saveEntries()
+    })
 
     const options = reactive({
       sort: false
@@ -43,7 +47,7 @@ export const useStoreEntries = defineStore('entries', () => {
   /*
     getters
   */
-
+  
     const balance = computed(() => {
       return entries.value.reduce((accumulator, { amount }) => {
         return accumulator + amount
@@ -75,7 +79,7 @@ export const useStoreEntries = defineStore('entries', () => {
   /*
     actions
   */
-
+  
     const addEntry = addEntryForm => {
       const newEntry = Object.assign({}, addEntryForm, { id: uid(), paid: false })
       entries.value.push(newEntry)
@@ -84,6 +88,7 @@ export const useStoreEntries = defineStore('entries', () => {
     const deleteEntry = entryId => {
       const index = getEntryIndexById(entryId)
       entries.value.splice(index, 1)
+      removeSlideItemIfExists(entryId)
       Notify.create({
         message: 'Entry deleted',
         position: 'top'
@@ -101,21 +106,42 @@ export const useStoreEntries = defineStore('entries', () => {
       entries.value.splice(newIndex, 0, movedEntry)
     }
 
+    const saveEntries = () => {
+      LocalStorage.set('entries', entries.value)
+    }
+
+    const loadEntries = () => {
+      const savedEntries = LocalStorage.getItem('entries')
+      if (savedEntries) Object.assign(entries.value, savedEntries)
+    }
+
 
   /*
     helpers
   */
-
+  
     const getEntryIndexById = entryId => {
       return entries.value.findIndex(entry => entry.id === entryId)
+    }
+
+    const removeSlideItemIfExists = entryId => {
+      // hacky fix: when deleting (after sorting),
+      // sometimes the slide item is not removed
+      // from the dom. this will remove the slide
+      // item from the dom if it still exists
+      // (after entry removed from entries array)
+      nextTick(() => {
+        const slideItem = document.querySelector(`#id-${ entryId }`)
+        if (slideItem) slideItem.remove()
+      })
     }
 
 
   /*
     return
   */
-
-    return {
+  
+    return { 
 
       // state
       entries,
@@ -130,8 +156,9 @@ export const useStoreEntries = defineStore('entries', () => {
       addEntry,
       deleteEntry,
       updateEntry,
-      sortEnd
+      sortEnd,
+      loadEntries
 
     }
-
+    
 })
